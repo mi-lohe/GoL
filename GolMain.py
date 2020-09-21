5524f2073a2f8c2ce77cecc479089ef6a43eaf4c
mi-lohe <71154550+mi-lohe@users.noreply.github.com>
2020-09-21 11:00:09 +0200

    CylinderWorld UPdate

--- GolMain.py
+++ GolMain.py
@@ -3,68 +3,48 @@ from colorama import init,Fore
 import os
 from copy import deepcopy
 import random
+import keyboard 
 
 
-init()
-LargMatrice = 100
-HautMatrice = 50
-simActive = "INFINITE" # ON:Lance la simulation avec un temp fini/OFF:Lance que la première étape/INFINITE:la simulation ne s'arrête jamais 
-nbrIteration = 0
-blyat=0
-ITERATIONMAX = 400
-
-TxtStart = open("C:\\Users\\azertyuiop\\Documents\\GitHub\\TP info\\GoL\\Start.txt")
-Lines = TxtStart.readlines()
-for Line in Lines:
-    print(Line)
-
-Randomisation = input("Random ?")
-
-
-
-
-if Randomisation == "Y" : # demande si on commence le jeu avec une partie randomisée ou non 
-    MatriceMonde = [[ bool(random.getrandbits(1)) for i in range(HautMatrice)] for j in range(LargMatrice)]
-else:
-    MatriceMonde = [[ False for i in range(HautMatrice)] for j in range(LargMatrice)]
-
-MemMatrice = deepcopy(MatriceMonde)
-MatricePrec = [[ False for i in range(HautMatrice)] for j in range(LargMatrice)]
-
-for y in range(0,HautMatrice):
-    for x in range(0,LargMatrice):
-        MatricePrec[x][y] = not MatriceMonde[x][y]
+def TxttoMatrice (Matr) : #injecte le contenu d'un txt. dans une liste booleenne de deux dimensions "X"=true else =false
+    with open("C:\\Users\\azertyuiop\\Documents\\GitHub\\TP info\\GoL\\Start.txt",'r') as TxtStart :
+        Lines = TxtStart.readlines()
+        for indiceLigne in range (len(Lines)):
+            for indiceCarac in range(len(Lines[indiceLigne] )):
+                Carac = Lines[indiceLigne][indiceCarac]
+                if Carac == "X" :
+                    Matr[indiceCarac+4][indiceLigne+4] = True
+                else :
+                    Matr[indiceCarac+4] [indiceLigne+4]= False
+    TxtStart.closed
 
 def Vaisseau(modele,x,y): # Fonction chargée de faire apparaitre un vaiseau en mode non randomisé
-    if Randomisation != 'Y':
-        if modele == "planneur" : 
+    if modele == "planneur" : 
 
-            MatriceMonde[x][y] = True
-            MatriceMonde[x+1][y] = True
-            MatriceMonde[x+2][y] = True
-            MatriceMonde[x+2][y+1] = True
-            MatriceMonde[x+1][y+2] = True
-        elif modele == "LWSS" :
-            MatriceMonde[x][y+1] = True
-            MatriceMonde[x][y+3] = True
-            MatriceMonde[x+1][y] = True
-            MatriceMonde[x+2][y] = True
-            MatriceMonde[x+3][y] = True
-            MatriceMonde[x+3][y+3] = True
-            MatriceMonde[x+4][y] = True
-            MatriceMonde[x+4][y+1] = True
-            MatriceMonde[x+4][y+2] = True
-        elif modele == "point":
-            MatriceMonde[x][y]=True
-        elif modele == "oscilateur 2P":
-            MatriceMonde[x][y] = True
-            MatriceMonde[x][y+1] = True
-            MatriceMonde[x][y+2] = True
-
+        MatriceMonde[x][y] = True
+        MatriceMonde[x+1][y] = True
+        MatriceMonde[x+2][y] = True
+        MatriceMonde[x+2][y+1] = True
+        MatriceMonde[x+1][y+2] = True
+    elif modele == "LWSS" :
+        MatriceMonde[x][y+1] = True
+        MatriceMonde[x][y+3] = True
+        MatriceMonde[x+1][y] = True
+        MatriceMonde[x+2][y] = True
+        MatriceMonde[x+3][y] = True
+        MatriceMonde[x+3][y+3] = True
+        MatriceMonde[x+4][y] = True
+        MatriceMonde[x+4][y+1] = True
+        MatriceMonde[x+4][y+2] = True
+    elif modele == "point":
+        MatriceMonde[x][y]=True
+    elif modele == "oscilateur 2P":
+        MatriceMonde[x][y] = True
+        MatriceMonde[x][y+1] = True
+        MatriceMonde[x][y+2] = True
 
 def CursorPos(x,y): #Place le curseur sur une position x , y spécifié
     print("\x1b["+str(y)+";"+str(x)+"H",end="")
-
 
 def AffMatrice1 (Matr): #méthode d'affichage lente et obsolète
     os.system("cls")
@@ -75,7 +55,7 @@ def AffMatrice1 (Matr): #méthode d'affichage lente et obsolète
             else :
                 print(Fore.YELLOW + "██",end="")
         print("")
-        
+
 def AffMatrice2 (Matr):#méthode d'affichage par balayage mais sûre
     for g in range (1,HautMatrice-1):
         for h in range (1,LargMatrice-1):
@@ -97,10 +77,10 @@ def AffMatrice3 (Matr):#méthode d'affiche ultra rapide si peu de cellule , mais
                     CursorPos((h*2),g)
                     print(Fore.YELLOW +"██")
 
-def AffMatrice4 (Matr):#méthode d'affiche ultra rapide si peu de cellule 
-    for g in range (1,HautMatrice-1):
-        for h in range (1,LargMatrice-1):
-            if (combVoisin(Matr,h,g) > 0 ) or (MatriceMonde[h][g] != MatricePrec[h][g]) :
+def AffMatrice4 (Matr,MatrPrec):#méthode d'affiche ultra rapide si peu de cellule 
+    for g in range (0,HautMatrice):
+        for h in range (0,LargMatrice):
+            if (combVoisin2(Matr,h,g,LargMatrice,HautMatrice,True) > 0 ) or (Matr[h][g] != MatrPrec[h][g]) :
                 if Matr[h][g] == True:
                     CursorPos((h*2),g)
                     print(Fore.BLUE + "██")
@@ -112,50 +92,128 @@ def AffDebug(Matr):
     AffMatrice4(Matr)
     for g in range (1,HautMatrice-1):
         for h in range (LargMatrice-1):
-            print(combVoisin(MemMatrice,h,g)," ",end="")
+            print(combVoisin2(MemMatrice,h,g,LargMatrice,HautMatrice,False)," ",end="")
         print("")
 
 def combVoisin(h,m,n):
     return  h[m-1][n-1] + h[m-1][n] + h[m-1][n+1] + h[m][n-1] + h[m][n+1] + h[m+1][n-1] + h[m+1][n] + h[m+1][n+1] 
 
-def combVoisin2(h,m,n):
+def combVoisinCent(h,m,n):
     return  h[m-1][n-1] + h[m-1][n] + h[m-1][n+1] + h[m][n-1] + h[m][n+1] + h[m+1][n-1] + h[m+1][n] + h[m+1][n+1] + h[m][n]
 
+def combVoisin2(h,m,n,mm,nm,CenterInclude):
+    s = 0
+    for j in range (-1,2,1):
+        for i in range(-1,2,1):
+            if (h[(m+j) % mm][(n+i) % nm] == True) and (CenterInclude  or not((i == 0) and (j == 0))):
+             s = s + 1
 
-#Vaisseau ("planneur", 4,40)
-Vaisseau ("LWSS",15,30)
-Vaisseau ("point",25,20)
-Vaisseau ("LWSS",15,10)
-AffMatrice4(MatriceMonde)
-while simActive == "ON" or simActive == "INFINITE":
-    #time.sleep(1)
-    
-    nbrIteration += 1
-    #print("nop", nbrIteration,combVoisin(MatriceMonde,6,5))
-    if (nbrIteration >= ITERATIONMAX) and (simActive == "ON")  :
-        simActive = "OFF" 
-    
-    MemMatrice = deepcopy(MatriceMonde)
-    #AffDebug()
+    return s
 
+def CalcGoL():
     
-    #print("NbrIteration", nbrIteration,combVoisin(MatriceMonde,4,5))
-    
-    
-    MatricePrec = deepcopy(MatriceMonde)
-    for x in range (1,LargMatrice-1):
-        for y in range(1,HautMatrice-1):
+    for x in range(0,LargMatrice):
+        for y in range(0,HautMatrice):
             
-            if (combVoisin(MemMatrice,x,y) == 3 ) and (not MemMatrice[x][y]):
+            if (combVoisin2(MemMatrice,x,y,LargMatrice,HautMatrice,False) == 3 ) and (not MemMatrice[x][y]):
                 MatriceMonde[x][y] = True
             
-            elif(((combVoisin(MemMatrice,x,y) == 3) or (combVoisin(MemMatrice,x,y) == 2) )) and MemMatrice[x][y]:
+            elif( (combVoisin2(MemMatrice,x,y,LargMatrice,HautMatrice,False) == 3) or (combVoisin2(MemMatrice,x,y,LargMatrice,HautMatrice,False) == 2) ) and (MemMatrice[x][y] == True):
                 MatriceMonde[x][y] = True
             else : 
                 MatriceMonde[x][y] = False
-               #time.sleep(0.1)
+            #time.sleep(0.1)
             #AffMatrice()
-    AffMatrice4(MatriceMonde)
+
+init()
+LargMatrice = 100
+HautMatrice = 50
+simActive = "INFINITE" # ON:Lance la simulation avec un temp fini/OFF:Lance que la première étape/INFINITE:la simulation ne s'arrête jamais 
+nbrIteration = 0
+ModeChargement = "J"
+ITERATIONMAX = 400
+enPause = False
+
+
+while (ModeChargement != 'R') and (ModeChargement != 'N') and (ModeChargement != 'T') :
+    os.system("cls")
+    
+    for i in range (int(LargMatrice/2)-20,int(LargMatrice/2)+20) :
+        CursorPos(i,8)
+        print("-")
+    
+    CursorPos(int(LargMatrice/2)-22,10)
+    print("Mode R : Placement randomisée des cellules")
+    CursorPos(int(LargMatrice/2)-22,11)
+    print('Mode N : Placement "hard coded" des cellules')
+    CursorPos(int(LargMatrice/2)-22,12)
+    print("Mode T : Placement des cellules en fonction de Start.txt")
+    CursorPos(int(LargMatrice/2)-22,15)
+    ModeChargement = input("Réponse (R/N/T) :")
+    print(ModeChargement)
+    
+MatriceMonde = [[ False for i in range(HautMatrice)] for j in range(LargMatrice)]
+
+if ModeChargement == "R" : # demande si on commence le jeu avec une partie randomisée ou non 
+    MatriceMonde = [[ bool(random.getrandbits(1)) for i in range(HautMatrice)] for j in range(LargMatrice)]
+elif ModeChargement == "N":
+    MatriceMonde = [[ False for i in range(HautMatrice)] for j in range(LargMatrice)]
+elif ModeChargement == "T":
+    TxttoMatrice(MatriceMonde) #Injection du txt dans la matrice principale
+
+#initialisation des Matrices : 
+MatricePrec = [[ False for i in range(HautMatrice)] for j in range(LargMatrice)]
+
+MemMatrice = deepcopy(MatriceMonde)
+
+
+if ModeChargement == 'N':
+    Vaisseau ("planneur", 4,40)
+    Vaisseau ("LWSS",15,30)
+    Vaisseau ("point",25,20)
+    Vaisseau ("LWSS",15,10)
+
+#Creation d'une matrice à  n-1 étape et inversé pour forcer la MAJ complète de l'écran 
+for y in range(0,HautMatrice):
+    for x in range(0,LargMatrice):
+        MatricePrec[x][y] = not MatriceMonde[x][y]
+
+
+AffMatrice4(MatriceMonde,MatricePrec)
+while (simActive == "ON") or (simActive == "INFINITE"): #Boucle principale 
+    #time.sleep(1)
+    
+    nbrIteration += 1
+
+    if (nbrIteration >= ITERATIONMAX) and (simActive == "ON")  :
+        simActive = "OFF" 
+    
+    if keyboard.is_pressed('q'):
+        simActive = "OFF"
+
+    if keyboard.is_pressed('p'):
+        enPause = True
+
+    if keyboard.is_pressed('r'):
+        enPause = False
+    
+    MemMatrice = deepcopy(MatriceMonde)
+    #AffDebug()
+    
+    if enPause == False :
+        MatricePrec = deepcopy(MatriceMonde)
+
+        CalcGoL()
+        AffMatrice4(MatriceMonde,MatricePrec)
+    #MatriceMonde[5][5] = True
+    #MatriceMonde[5][6] = True
+    #MatriceMonde[4][5] = True
+    #print(combVoisin3(MatriceMonde,4,5,LargMatrice,HautMatrice))
+
+os.system("cls")
+print("GOODBYE")
+time.sleep(1)
+os.system("cls")
     
     
     
